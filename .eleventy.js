210a506fc19919f34cc58eb6026918932b3cf7d0
Juan Bernardo Benavides Rubio <juan.benavides@fintegra.co>
2022-01-08 12:36:36 -0500

    Refactor code and get page ready to recieve multiple editions of academy

--- .eleventy.js
+++ .eleventy.js
@@ -4,15 +4,9 @@ const CleanCSS = require("clean-css");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
 
-    eleventyConfig.addCollection("orderedProjects", function (collection) {
-        return collection.getFilteredByTag("projects").sort((a, b) => {
-            return a.data.order - b.data.order;
-        });
-    });
-
-    eleventyConfig.addFilter("cssmin", function(code) {
+    eleventyConfig.addFilter("cssmin", function (code) {
         return new CleanCSS({}).minify(code).styles;
-      });
+    });
 
     return {
         dir: {
---
+++ src/_data/academies.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        title: "Structure and Scale - Jan 2022",
+        description:
+            "This is the next level of the academy, it is a shorter edition focused on going beyond the basics and learning to structure and organize my code as projects start to grow.",
+        tagName: "jan2022",
+    },
+    {
+        title: "Vanilla JS Academy - June 2021",
+        description:
+            "My first academy. Here I went from basic knowledge of javascript to having a solid understanding that left me ready for my first job as a developer",
+        tagName: "june2021",
+    },
+];
